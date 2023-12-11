e2accfb13ba562a0544b51549d62dfef7033ff5e
lcygler <leandrocygler@gmail.com>
2023-12-11 20:58:16 -0300

    update script

--- 20231211-JavaScript/promises/weather-channel/js/script.js
+++ 20231211-JavaScript/promises/weather-channel/js/script.js
@@ -1,25 +1,53 @@
+// Obtener elementos
 const input = document.getElementById("input");
 const enviar = document.getElementById("enviar");
 const ciudad = document.getElementById("ciudad");
 const temperatura = document.getElementById("temperatura");
 const wicon = document.getElementById("wicon");
 const descripcion = document.getElementById("descripcion");
+const container = document.querySelector(".container");
 
+// Agregar eventos
 enviar.addEventListener("click", getWeather);
 
+input.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    getWeather();
+  }
+});
+
+// Obtener clima
 function getWeather() {
-  console.log(input.value);
+  const inputValue = input.value.trim();
+
+  // Ciudad vacía
+  if (inputValue === "") {
+    alert("Por favor, ingrese una ciudad.");
+    return;
+  }
+
   fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${input.value}&units=metric&appid=fdd533266e28101881f610f2b8f1ebe1`
+    `https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&units=metric&appid=fdd533266e28101881f610f2b8f1ebe1`
   )
     .then((response) => response.json())
     .then((data) => {
-      console.log("API Response:", data);
+      // Respuesta API
+      console.log("Respuesta API:", data);
+
+      // Ciudad inválida
+      if (data.cod === "404") {
+        alert("Por favor, ingrese una ciudad válida.");
+        input.value = "";
+        return;
+      }
+
+      // Actualizar elementos
       ciudad.textContent = data.name;
       temperatura.innerHTML = data.main.temp + "<sup>°C</sup>";
       wicon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
       descripcion.textContent = data.weather[0].description;
-      document.querySelector(".container").style.visibility = "visible";
+      container.style.visibility = "visible";
+      input.value = "";
     })
     .catch((error) => {
       console.error("Ocurrió un error:", error);
