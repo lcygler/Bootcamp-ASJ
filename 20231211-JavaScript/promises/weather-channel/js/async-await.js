eee6326f22908e19dadd4942b6ce496f8e437b87
lcygler <leandrocygler@gmail.com>
2023-12-12 09:46:35 -0300

    add async/await

---
+++ 20231211-JavaScript/promises/weather-channel/js/async-await.js
@@ -0,0 +1,58 @@
+// API
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather";
+const apiKey = "fdd533266e28101881f610f2b8f1ebe1";
+
+// Obtener elementos
+const input = document.getElementById("input");
+const enviar = document.getElementById("enviar");
+const ciudad = document.getElementById("ciudad");
+const temperatura = document.getElementById("temperatura");
+const wicon = document.getElementById("wicon");
+const descripcion = document.getElementById("descripcion");
+const container = document.querySelector(".container");
+
+// Agregar eventos
+enviar.addEventListener("click", getWeather);
+
+input.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    getWeather();
+  }
+});
+
+// Obtener clima
+async function getWeather() {
+  const inputValue = input.value.trim();
+
+  // Ciudad vacía
+  if (inputValue === "") {
+    alert("Por favor, ingrese una ciudad.");
+    return;
+  }
+
+  try {
+    const response = await fetch(`${apiUrl}?q=${inputValue}&units=metric&appid=${apiKey}`);
+
+    const data = await response.json();
+
+    // Respuesta API
+    console.log("Respuesta API:", data);
+
+    // Ciudad inválida
+    if (data.cod === "404") {
+      alert("Por favor, ingrese una ciudad válida.");
+      input.value = "";
+      return;
+    }
+
+    // Actualizar elementos
+    ciudad.textContent = data.name;
+    temperatura.innerHTML = data.main.temp.toFixed(1) + "<sup>°C</sup>";
+    wicon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    descripcion.textContent = data.weather[0].description;
+    container.style.visibility = "visible";
+    input.value = "";
+  } catch (error) {
+    console.error("Ocurrió un error:", error);
+  }
+}
