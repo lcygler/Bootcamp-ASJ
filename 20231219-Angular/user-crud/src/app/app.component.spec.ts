625a54b185382345d55e41f5f6d8cdc1a6cdf232
lcygler <leandrocygler@gmail.com>
2023-12-19 19:09:46 -0300

    rename project

--- 20231219-Angular/user-crud/src/app/app.component.spec.ts
+++ 20231219-Angular/user-crud/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ejemploUsers'`, () => {
+  it(`should have as title 'user-crud'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ejemploUsers');
+    expect(app.title).toEqual('user-crud');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ejemploUsers app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('user-crud app is running!');
   });
 });
