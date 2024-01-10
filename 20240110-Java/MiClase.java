542f2c62bae6220a97bfd36a4e119b7b49867833
lcygler <leandrocygler@gmail.com>
2024-01-10 18:11:52 -0300

    add java exercise

---
+++ 20240110-Java/MiClase.java
@@ -0,0 +1,39 @@
+
+public class MiClase {
+
+	public static void main(String[] args) {
+		System.out.println("Hola!");
+
+		// Declaración de variables
+		int edad;
+		float precio;
+		double total;
+		boolean estado;
+		char letra;
+		String nombre;
+
+		// Inicialización de variables
+		edad = 28;
+		precio = 9.99f;
+		total = 99.99; 
+		estado = true;
+		letra = 'L';
+		nombre = "Leandro";
+
+		// Uso de variables
+		System.out.println("Nombre: " + nombre);
+		System.out.println("Edad: " + edad);
+		System.out.println("Estado: " + estado);
+		System.out.println("Precio: " + precio);
+		System.out.println("Total: " + total);
+		System.out.println("Letra: " + letra);
+
+		// Estructuras condicionales
+		if (edad > 18) {
+			System.out.println("Es mayor de edad");
+		} else {
+			System.out.println("Es menor de edad");
+		}
+	}
+
+}
