0260948e60c32806d06b911ae2ac527b2fcfcfb8
lcygler <leandrocygler@gmail.com>
2024-03-04 20:47:03 -0300

    update cpp example

--- 20240228-CPP/primer-programa.cpp
+++ 20240228-CPP/primer-programa.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
+  // cout << "Hola ";
+  // cout << "Mundo!" << endl;
+  // std::cout << "Hola Mundo!" << std::endl;
+
   cout << "Hola Mundo!" << endl;
+
+  int salario = 1000;
+  cout << "Salario: $" << salario << endl;
+
   return 0;
 }
